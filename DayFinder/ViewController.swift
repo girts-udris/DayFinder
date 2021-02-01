beb98580f32895f9f2522a09de498852cb3c5830
Girts-Udris <girts.udris@accenture.com>
2021-02-01 14:29:04 +0200

    Initial Commit

---
+++ DayFinder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DayFinder
+//
+//  Created by girts.udris on 01/02/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
